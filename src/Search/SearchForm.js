018049ecddfb96b78482ea7105b98a6491595914
Gaurav Thakur <gthakur581@gmail.com>
2020-12-04 09:10:52 +0530

    🔍 Added lazy search support. It will automatically search as you start typing and when query is empty it will use the default term "pizza" as search item

--- src/Search/SearchForm.js
+++ src/Search/SearchForm.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Input from '../shared/Input';
 import Button from '../shared/Button';
 
-const SearchForm = ({ handleSearch }) => {
+const SearchForm = ({handleSearch}) => {
   const [query, setQuery] = useState('');
 
   const handleOnChange = (e) => {
     const query = e.target.value;
     setQuery(query);
   };
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => handleSearch(query ? query : 'pizza'), 1000)
+    return () => clearTimeout(timeOut)
+  }, [query])
 
   return (
     <div
@@ -20,7 +25,7 @@ const SearchForm = ({ handleSearch }) => {
       }}
     >
       <Input
-        style={{ width: '100%' }}
+        style={{width: '100%'}}
         value={query}
         onChange={handleOnChange}
         placeholder='Search Your Fav Recipe...'
